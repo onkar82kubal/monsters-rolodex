9089e1d881696cba6dc1e483accc1326f3c9a9e0
Onkar.Kubal <onkar.kubal@ril.com>
2020-03-11 17:10:16 +0530

    Updates

---
+++ precache-manifest.f684780cc5f7b5b991476c592fe0b768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a4039837f9b63d0c0de3b195fb0de1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "017ae8e51e2a8e73cf63",
+    "url": "/monsters-rolodex/static/css/main.db7608da.chunk.css"
+  },
+  {
+    "revision": "c9a0510c7c25fecaaaab",
+    "url": "/monsters-rolodex/static/js/2.404a6193.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.404a6193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017ae8e51e2a8e73cf63",
+    "url": "/monsters-rolodex/static/js/main.55034781.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
